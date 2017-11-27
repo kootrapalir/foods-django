b2604d935cc91263d1c886c3a9afc3621397314e
shreyam1008 <shreyam1008@gmail.com>
2017-11-27 11:28:09 +0545

    templete context

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from restaurants.views import home
+from restaurants.views import home, home2, home3
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -24,4 +24,7 @@ urlpatterns = [
     #getting home function to default url from browser
     #absolute home url 127.0.0.1:8000
     url(r'^$', home),
+    url(r'^home2$', home2),
+    url(r'^home3$', home3),
+
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,7 @@
 #to return to html use ..return render()
 #render takes 3 arguments, (request, 'templete", {dictionary: "context}
-from django.shortcuts import render
+
+
 
 #for simple http response right here and now ue this module
 from django.http import HttpResponse
@@ -9,9 +10,26 @@ from django.http import HttpResponse
 
 #creating views
 #function based view
+from django.shortcuts import render
+
+
 def home(request):
 
     bdd = "Vishaka"
     mdd = "Shyama"
     cdd = "Krishna"
-    return render(request, "base.html", {"bdd": bdd, "mdd": mdd, "cdd": cdd})
+    list_ = [bdd, mdd, cdd]
+    context = {"dds": list_}
+    return render(request, "base.html", context)
+
+def home2(request):
+
+
+    context = {}
+    return render(request, "home2.html", context)
+
+def home3(request):
+
+
+    context = {}
+    return render(request, "home3.html", context)
