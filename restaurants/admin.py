d96200d80690ae595976b1a07a14c53b0766f53e
shreyam1008 <shreyam1008@gmail.com>
2017-11-27 18:15:25 +0545

    making models

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+#to bring the model into admin page for manipulation
+from .models import ResturantLocation
+admin.site.register(ResturantLocation)
+
+
