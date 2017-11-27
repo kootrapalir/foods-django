df111e56eca83e8bcf813fce9c7c8009e1203bd9
shreyam1008 <shreyam1008@gmail.com>
2017-11-27 18:31:04 +0545

    models to admin site

--- restaurants/models.py
+++ restaurants/models.py
@@ -4,7 +4,16 @@ from django.db import models
 #models are for remembering things using database
 #creating type of values we need to store in database
 class ResturantLocation(models.Model):
-    name = models.CharField(max_length=120)
-    location = models.CharField(max_length=120, null=True, blank=True)
+    name        = models.CharField(max_length=120)
+    location    = models.CharField(max_length=120, null=True, blank=False)
+    category    = models.CharField(max_length=120, null=True, blank=True)
+    #if auto_now and auto_now_add is true you cant edit tiome in database
+    # but its added automatically
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+
+    # my_date_field = models.DateField(auto_now_add=False, auto_now=False)
+
+
 
 
