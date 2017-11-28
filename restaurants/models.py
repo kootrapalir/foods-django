018dac3e6177b50af09afaa1d3a89badbe4100b9
shreyam1008 <shreyam1008@gmail.com>
2017-11-28 17:26:38 +1100

    slug generators and signals

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+#for for signals
+from django.db.models.signals import pre_save, post_save
+
+#with help of post-save and pre_save we generetare slug now
+from .utils import unique_slug_generator
+
 # Create your models here.
 #models are for remembering things using database
 #creating type of values we need to store in database
@@ -10,10 +16,10 @@ class RestaurantLocation(models.Model):
     #if auto_now and auto_now_add is true you cant edit tiome in database
     # but its added automatically
     timestamp   = models.DateTimeField(auto_now_add=True)
-    updated     = models.DateTimeField(auto_now=True)
+     updated     = models.DateTimeField(auto_now=True)
 
     #making slug bull and blank inly here...otherwise create it in start and make it unique=True
-    slug        = models.SlugField(null=True, blank=False)
+    slug        = models.SlugField(null=True, blank=True)
 
     # my_date_field = models.DateField(auto_now_add=False, auto_now=False)
 
@@ -26,5 +32,22 @@ class RestaurantLocation(models.Model):
         return self.name
 
 
+#these 2 functions are for doing this during and after the new object is being saving or save
+#but prefer to do it in during save as its safe
+#function to do during save
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
 
+# #not so safe to save after save
+# #function to do after save
+# def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
+#     print("saved")
+#     print(instance.timestamp)
+#     if not instance.slug:
+#         instance.slug = unique_slug_generator(instance)
+#         instance.save()
+#connectiong these saving functions to save and saving moments
+pre_save.connect(rl_pre_save_receiver, sender = RestaurantLocation)
+# post_save.connect(rl_post_save_receiver, sender = RestaurantLocation)
 
